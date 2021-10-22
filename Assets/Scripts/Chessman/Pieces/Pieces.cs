bd18d64479d4bf2804be02906e38d940ddc98696
Losonczi Tamás <losonczi.tamas91@gmail.com>
2021-10-22 22:01:26 +0200

    add dark pawn piece at setup

--- Assets/Scripts/Chessman/Pieces/Pieces.cs
+++ Assets/Scripts/Chessman/Pieces/Pieces.cs
@@ -19,11 +19,15 @@ namespace Chessman.Pieces
         {
             var bottomLeftTile = _tileContainer.GetTile(Vector2Int.zero);
             
-            var pawn = Instantiate(_pawnPrefab, bottomLeftTile.transform.position, Quaternion.identity, transform);
-            pawn.Init(Vector2Int.zero, PieceColor.Light);
-            bottomLeftTile.ChessPiece = pawn;
+            var lightPawn = Instantiate(_pawnPrefab, bottomLeftTile.transform.position, Quaternion.identity, transform);
+            lightPawn.Init(bottomLeftTile.Position, PieceColor.Light);
+            bottomLeftTile.ChessPiece = lightPawn;
             
+            var topLeftTile = _tileContainer.GetTile(new Vector2Int(0, 7));
             
+            var darkPawn = Instantiate(_pawnPrefab, topLeftTile.transform.position, Quaternion.identity, transform);
+            darkPawn.Init(topLeftTile.Position, PieceColor.Dark);
+            topLeftTile.ChessPiece = darkPawn;
         }
     }
 }
