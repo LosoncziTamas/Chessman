19501c0844f174164fd51abb5e63b3c349bea0ff
Losonczi Tamás <losonczi.tamas91@gmail.com>
2021-11-08 21:41:08 +0100

    tweak panel animation

---
+++ Assets/Scripts/Chessman/GUI/Panel.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Chessman.GUI
+{
+    public class Panel : MonoBehaviour
+    {
+        private static readonly int Property = Animator.StringToHash("Panel Open");
+
+        [SerializeField] private Animator _animator;
+
+        private void Awake()
+        {
+        }
+
+        private void OnGUI()
+        {
+            var opened = _animator.GetBool(Property);
+            if (GUILayout.Button(opened ? "Close" : "Open"))
+            {
+                _animator.SetBool(Property, !opened);
+            }
+        }
+    }
+}
