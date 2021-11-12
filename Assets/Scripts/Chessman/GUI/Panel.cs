1a6432e4e9c81e81e918f36b67a5265b058c9448
Losonczi Tamás <losonczi.tamas91@gmail.com>
2021-11-12 17:48:12 +0100

    tweak GUI

--- Assets/Scripts/Chessman/GUI/Panel.cs
+++ Assets/Scripts/Chessman/GUI/Panel.cs
@@ -1,26 +1,72 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static System.String;
 
 namespace Chessman.GUI
 {
     public class Panel : MonoBehaviour
     {
-        private static readonly int Property = Animator.StringToHash("Panel Open");
+        private static readonly int PanelOpen = Animator.StringToHash("Panel Open");
+
+        private const string WonText = "{0} player won!";
 
         [SerializeField] private Animator _animator;
+        [SerializeField] private TextMeshProUGUI _mainText;
+        [SerializeField] private Button _retryButton;
+        [SerializeField] private CanvasGroup _canvasGroup;
 
-        private void Awake()
+        private void Start()
         {
-            
+            SetInteractable(false);
+        }
+
+        private void OnEnable()
+        {
+            _retryButton.onClick.AddListener(OnRetryClicked);
         }
 
         private void OnGUI()
         {
-            var opened = _animator.GetBool(Property);
-            if (GUILayout.Button(opened ? "Close" : "Open"))
+            if (GUILayout.Button("Show"))
             {
-                _animator.SetBool(Property, !opened);
+                ShowPanel(PieceColor.Light);
             }
+            if (GUILayout.Button("Hide"))
+            {
+                HidePanel();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _retryButton.onClick.RemoveListener(OnRetryClicked);
+        }
+
+        private void OnRetryClicked()
+        {
+            
+            HidePanel();
+        }
+
+        public void ShowPanel(PieceColor winner)
+        {
+            _mainText.text = Format(WonText, winner);
+            _animator.SetBool(PanelOpen, true);
+            SetInteractable(true);
+        }
+
+        public void HidePanel()
+        {
+            _animator.SetBool(PanelOpen, false);
+            SetInteractable(false);
+        }
+        
+        private void SetInteractable(bool interactable)
+        {
+            _canvasGroup.interactable = interactable;
+            _canvasGroup.blocksRaycasts = interactable;
         }
     }
 }
